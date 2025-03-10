5d312e2b3a11010cd9f0db81dfb24d909a2bb1e9
cookiegigi <gcreusot@pm.me>
2025-03-11 02:57:37 +0800

    fix: npm package installation

--- roles/node/files/node_install.sh
+++ roles/node/files/node_install.sh
@@ -7,6 +7,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 # Download and install Node.js:
 nvm install 22
 
+sudo ln -s $(which npm) /usr/local/bin/npm
+sudo ln -s $(which node) /usr/local/bin/node
+
 # Verify the Node.js version:
 node -v # Should print "v22.14.0".
 nvm current # Should print "v22.14.0".
